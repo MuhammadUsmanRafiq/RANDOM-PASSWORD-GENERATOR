d4a1e71030d93f12451ec7f9321361a425111c33
USER <USER@DESKTOP-EHNDH1H>
2024-07-10 20:25:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@$%^&*_~!><?";
+
+const allChars = upperCase + lowerCase + number + symbol;
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCaseCase.length)];
+    password += upperCase[Math.floor(Math.random() * number.length)];
+    password += upperCase[Math.floor(Math.random() * symbol.length)];
+  while(length > password.length){
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+   passwordBox.value = password;
+}
